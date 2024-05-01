021049ee3cbdee61b74c406e6be934fd1166a9de
Awadh-Awadh <awadhawadh84@yahoo.com>
2024-05-01 09:02:32 +0300

    Add JsonParser project

---
+++ LanguageMaster.JsonParser/Lexer.cs
@@ -0,0 +1,9 @@
+namespace LanguageMaster.JsonParser;
+
+public class Lexer
+{
+    public static string[] Scanner(string jsonString)
+    {
+        return new[] { "test" };
+    }
+}
---
+++ LanguageMaster.JsonParser/Parser.cs
@@ -0,0 +1,9 @@
+namespace LanguageMaster.JsonParser;
+
+public class Parser
+{
+    public static bool IsValidJson(string tokens)
+    {
+        return true;
+    }
+}
