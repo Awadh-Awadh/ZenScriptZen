48869b765d14be55595268de0cece1b94a6ff7c4
Awadh-Awadh <awadhawadh84@yahoo.com>
2024-06-04 08:12:18 +0300

    add lexer and parser classes

--- LanguageMaster.JsonParser/Lexer.cs
+++ LanguageMaster.JsonParser/Lexer.cs
@@ -4,6 +4,6 @@ public class Lexer
 {
     public static string[] Scanner(string jsonString)
     {
-        return new[] { "test" };
+        return ["TestString"];
     }
 }
--- LanguageMaster.JsonParser/Parser.cs
+++ LanguageMaster.JsonParser/Parser.cs
@@ -6,4 +6,5 @@ public class Parser
     {
         return true;
     }
+    
 }
